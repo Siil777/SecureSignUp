2f4d174bc82495d183787a74fc654a711f3820df
Siil777 <pavel.ivanov.thk@gmail.com>
2024-10-26 20:17:27 +0300

    feat: form functionality

--- frontend/auth/src/components/signupforms.js
+++ frontend/auth/src/components/signupforms.js
@@ -25,16 +25,16 @@ const SignUpForm = ({ onRegister }) => {
             <form onSubmit={handleSubmit} style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', }}>
                 <FormControl margin="normal">
                     <InputLabel htmlFor='email'>Email address</InputLabel>
-                    <Input id="email" type="email" aria-describedby="text" value={email} onChange={(e) => setEmail(e.target.value)}></Input>
+                    <Input id="email" type="email" aria-describedby="text" value={email} onChange={(e) => setEmail(e.target.value)} required></Input>
                     <FormHelperText id='text'>We never share your email before it profitable for us</FormHelperText>
                 </FormControl>
                 <FormControl margin="normal">
                     <InputLabel htmlFor='username'>Username</InputLabel>
-                    <Input id="username" value={username} onChange={(e) => setUsername(e.target.value)}></Input>
+                    <Input id="username" value={username} onChange={(e) => setUsername(e.target.value)} required></Input>
                 </FormControl>
                 <FormControl margin="normal">
                     <InputLabel htmlFor='password'>Password</InputLabel>
-                    <Input id="password" value={password} onChange={(e) => setPassword(e.target.value)}></Input>
+                    <Input id="password" value={password} onChange={(e) => setPassword(e.target.value)} required></Input>
                 </FormControl>
                 <Button type="submit" color="secondary">Register</Button>
                 {showAlert && (
