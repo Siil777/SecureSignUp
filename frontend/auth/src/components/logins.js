b421b980c841bb5573af15023704c72923fd6655
Siil777 <pavel.ivanov.thk@gmail.com>
2024-10-26 16:18:49 +0300

    feat: working on logins component

---
+++ frontend/auth/src/components/logins.js
@@ -0,0 +1,18 @@
+
+import { Button } from "@mui/material";
+import {useNavigate} from 'react-router-dom';
+const SignIn = () => {
+
+    const navigate = useNavigate();
+
+    function handleRoute (){
+        	navigate('/logform')
+    }
+
+    return(
+        <Button onClick={handleRoute} color="white">
+            Sign in
+        </Button>
+    )
+}
+export default SignIn;
